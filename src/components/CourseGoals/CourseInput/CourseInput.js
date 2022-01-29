26869acb8fe9fab19b69fa1d669acd1c7b25d435
vuki247 <mateo.vukovic@tutamail.com>
2022-01-29 18:34:00 +0100

    Fix semicolon bug from last commit

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -10,14 +10,14 @@ const FormControl = styled.div`
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")}
+    color: ${(props) => (props.invalid ? "red" : "black")};
   }
 
   & input {
     display: block;
     width: 100%;
     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")}
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
     font: inherit;
     line-height: 1.5rem;
     padding: 0 0.25rem;
